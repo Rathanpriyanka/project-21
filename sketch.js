51255ce304918368d3ff216a588e4a24ba37e7fa
Rathanpriyanka <70433334+Rathanpriyanka@users.noreply.github.com>
2020-09-09 15:49:21 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,19 +7,22 @@ function setup() {
   bullet = createSprite(100,50,20,40);
   wall = createSprite(1200,200,thickness,height/2);
   wall.shapeColor= color(80,80,80)
-  speed.Math.round(random(223,321));
-  weight.Math.round(random(30,52));
-  thickness.Math.round(random(23,83));
+  speed=Math.round(random(223,321));
+  weight=Math.round(random(30,52));
+  thickness=Math.round(random(23,83));
 }
 
 function draw() {
   background(255,255,255);  
   if(keyDown("right")) {
-     bullet.velocityX=6;
+     bullet.velocityX=speed;
   }
+  
   if(hasCollided(bullet,wall)){
     bullet.velocityX=0;
-    var damage = (0.5*weight*speed*speed)/thickness*thickness*thickness;
+    
+    var damage = (0.5*weight*speed*speed)/(thickness*thickness*thickness);
+    console.log(damage);
     if(damage<10) {
         bullet.shapeColor = "green";
      }
@@ -32,10 +35,12 @@ function draw() {
 }
 
 function hasCollided(o1,o2) {
-   if(o2.x - o1.x (o1.width/2 + o2.width/2)) {
+   if(o2.x - o1.x <(o1.width/2 + o2.width/2)) {
        return  true;
    }
    else {
      return false;
    }
-}
+  }
+
+
